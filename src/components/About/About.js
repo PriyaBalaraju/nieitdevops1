4f567e10c0df590c2ea8c016b14b4a1382c9d28e
PriyaBalaraju <120739706+PriyaBalaraju@users.noreply.github.com>
2023-02-03 15:10:40 +0530

    spelling mistake

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -16,7 +16,7 @@ function About() {
         <p className='about-nike-para'>
           Our mission is what drives us to do everything possible to expand
           human potential. We do that by creating groundbreaking sport
-          innovations, by making our phhhhhhroducts more sustainably, by building a
+          innovations, by making our products  more sustainably, by building a
           creative and diverse global team and by making a positive impact in
           communities where we live and work. Based in Beaverton, Oregon, NIKE,
           Inc. includes the Nike, Converse, and Jordan brands.
